e33aa0d254ec5fd75ead312ccf2cda5289ea1481
kamrah334 <34321580+kamrah334@users.noreply.github.com>
2025-08-16 19:27:59 +0300

    Add files via upload

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,1 +1,116 @@
+<?php
+/**
+ * Custom template tags for this theme
+ *
+ * @package Shopora_Premium_Commerce
+ */
 
+if (!function_exists('shopora_posted_on')) :
+    /**
+     * Prints HTML with meta information for the current post-date/time.
+     */
+    function shopora_posted_on() {
+        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        if (get_the_time('U') !== get_the_modified_time('U')) {
+            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+        }
+
+        $time_string = sprintf($time_string,
+            esc_attr(get_the_date(DATE_W3C)),
+            esc_html(get_the_date()),
+            esc_attr(get_the_modified_date(DATE_W3C)),
+            esc_html(get_the_modified_date())
+        );
+
+        $posted_on = sprintf(
+            esc_html_x('Posted on %s', 'post date', 'shopora-premium-commerce'),
+            '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">' . $time_string . '</a>'
+        );
+
+        echo '<span class="posted-on">' . $posted_on . '</span>';
+    }
+endif;
+
+if (!function_exists('shopora_posted_by')) :
+    /**
+     * Prints HTML with meta information for the current author.
+     */
+    function shopora_posted_by() {
+        $byline = sprintf(
+            esc_html_x('by %s', 'post author', 'shopora-premium-commerce'),
+            '<span class="author vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
+        );
+
+        echo '<span class="byline"> ' . $byline . '</span>';
+    }
+endif;
+
+if (!function_exists('shopora_entry_footer')) :
+    /**
+     * Prints HTML with meta information for the categories, tags and comments.
+     */
+    function shopora_entry_footer() {
+        // Hide category and tag text for pages.
+        if ('post' === get_post_type()) {
+            /* translators: used between list items, there is a space after the comma */
+            $categories_list = get_the_category_list(esc_html__(', ', 'shopora-premium-commerce'));
+            if ($categories_list) {
+                /* translators: 1: list of categories. */
+                printf('<span class="cat-links">' . esc_html__('Posted in %1$s', 'shopora-premium-commerce') . '</span>', $categories_list);
+            }
+
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list('', esc_html_x(', ', 'list item separator', 'shopora-premium-commerce'));
+            if ($tags_list) {
+                /* translators: 1: list of tags. */
+                printf('<span class="tags-links">' . esc_html__('Tagged %1$s', 'shopora-premium-commerce') . '</span>', $tags_list);
+            }
+        }
+
+        if (!is_single() && !post_password_required() && (comments_open() || get_comments_number())) {
+            echo '<span class="comments-link">';
+            comments_popup_link(
+                sprintf(
+                    wp_kses(
+                        __('Leave a Comment<span class="screen-reader-text"> on %s</span>', 'shopora-premium-commerce'),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    get_the_title()
+                )
+            );
+            echo '</span>';
+        }
+
+        edit_post_link(
+            sprintf(
+                wp_kses(
+                    __('Edit <span class="screen-reader-text">"%s"</span>', 'shopora-premium-commerce'),
+                    array(
+                        'span' => array(
+                            'class' => array(),
+                        ),
+                    )
+                ),
+                get_the_title()
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+    }
+endif;
+
+if (!function_exists('wp_body_open')) :
+    /**
+     * Shim for sites older than 5.2.
+     *
+     * @link https://core.trac.wordpress.org/ticket/12563
+     */
+    function wp_body_open() {
+        do_action('wp_body_open');
+    }
+endif;
+?>
